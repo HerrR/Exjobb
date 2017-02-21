0c8291e3fcc1a7da69b75ce4cd6a9788e9162ae8
HerrR <rbergel@kth.se>
2017-02-21 11:06:10 +0100

    Basic accordion effect started

--- Assets/Scripts/layer.cs
+++ Assets/Scripts/layer.cs
@@ -5,10 +5,47 @@ using UnityEngine.UI;
 
 public class Layer : MonoBehaviour {
 	public GameObject mainCanvas;
+	public GameObject mainCamera;
+	public GameObject inspectionZone;
 	public Vector3 basePosition;
+	public bool accordion;
+
+	private float zMax;
+	private float zMin;
 
 	void Start () {
 		mainCanvas = GameObject.FindGameObjectWithTag ("MainCanvas");
+		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera");
+		inspectionZone = GameObject.FindGameObjectWithTag ("InspectionZone");
+		zMax = inspectionZone.GetComponent<Zone> ().bounds.zMax;
+		zMin = inspectionZone.GetComponent<Zone> ().bounds.zMin;
+		accordion = true;
+	}
+
+	void Update(){
+		if (accordion) {
+			AccordionMove ();
+		}
+	}
+
+	void AccordionMove(){
+		float OffsetPos = Mathf.Pow ((basePosition.z - mainCamera.transform.position.z), 2);
+		Vector3 _pos = gameObject.transform.position;
+		_pos.z = (basePosition.z + OffsetPos);
+		if (_pos.z > zMax) {
+			_pos.z = zMax;
+		}
+
+		if (_pos.z < zMin) {
+			_pos.z = zMin;
+		}
+
+		if (basePosition.z < mainCamera.transform.position.z) {
+			_pos.z = basePosition.z;
+		}
+
+		Debug.Log ("Pos: " + _pos.z + " ,Min: " + zMin + " ,Max: " + zMax , gameObject);
+		gameObject.transform.position = _pos;
 	}
 
 	public void copyToCanvas(){
