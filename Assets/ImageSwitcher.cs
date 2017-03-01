6928dda9c491157505dd8e415cc31dee346a1938
HerrR <rbergel@kth.se>
2017-03-01 12:39:20 +0100

    Outline for selected images

---
+++ Assets/ImageSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSwitcher : MonoBehaviour {
+
+	public Sprite defaultImage;
+	public Sprite activeImage;
+	public bool isSelected = false;
+	private Image targetImage;
+
+	void Start () {
+		targetImage = gameObject.GetComponent<Image> ();
+	}
+
+	void Update () {
+		if (isSelected && (targetImage.sprite == defaultImage)) {
+			switchImage ();
+		} else if (!isSelected && (targetImage.sprite == activeImage)){
+			switchImage ();
+		}
+	}
+
+	public void switchImage() {
+		if (isSelected) {
+			targetImage.sprite = activeImage;
+		} else {
+			targetImage.sprite = defaultImage;	
+		}
+	}
+}
